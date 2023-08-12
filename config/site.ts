02d791f18d0cf9e770d9abd897606c84e87c6902
Carlos Adames <92492690+KamajiRd@users.noreply.github.com>
2023-08-12 18:04:29 -0400

    Adjusted meta title

--- config/site.ts
+++ config/site.ts
@@ -1,8 +1,8 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-	name: "Next.js + NextUI",
-	description: "Make beautiful websites regardless of your design experience.",
+	name: "React Signature Form",
+	description: "Testing the Signature library for react",
 	navItems: [
 		{
 			label: "Home",
